716cd5408fc53d34284cf90aed4b2dccec61c483
AymaneElmahi <elmahi.aymane@gmail.com>
2023-07-07 14:06:13 +0200

    updated the utils

--- src/utils.py
+++ src/utils.py
@@ -29,48 +29,50 @@ def create_df(json_files):
 import numpy as np
 from datetime import timedelta
 
-def epoch_to_datetime(epoch_time):
+import datetime
+
+def epoch_to_datetime(epoch_time, h = 2):
     """
     Convert epoch time to datetime64 in GMT+2
     :param epoch_time: epoch time in milliseconds
+    :param h: int, number of hours to add
     :return: datetime64 in GMT+2
     """
     datetime = np.datetime64(epoch_time, 'ms')  # Convert to datetime64 in GMT
-    datetime += np.timedelta64(2, 'h')  # Add 2 hours for GMT+2
+    datetime += np.timedelta64(h, 'h')  # Add 2 hours for GMT+2
     return datetime
+
 
 import plotly.express as px
 
-def plot_time_series(df,y,title,matplotlib = False,first = True):
+def plot_time_series(df, y, title, matplotlib=False):
     """
     Plot time series of people count, plot png if matplotlib = True
     :param df: pandas dataframe
-    :param y : string, name of the column to plot
+    :param y: string, name of the column to plot
     :param matplotlib: boolean, if True plot png, else plot html
     :param first: boolean, if True, it is the first time to plot, else not
     :return: None
     """
-    if first:
-        # use .loc to apply pd.to_datetime to the column 'time'
-        df.loc[:,'time'] = pd.to_datetime(df['time']) 
-        
-    if matplotlib :
+    # Always convert the 'time' column to datetime
+    df.loc[:, 'time'] = pd.to_datetime(df['time'])
+
+    if matplotlib:
         # use matplotlib to plot
         fig, ax = plt.subplots()  # Create a figure and axes object
         ax.plot(df['time'], df[y])
-        ax.set(xlabel='time', ylabel=y, title='Time Series of '+title)
+        ax.set(xlabel='time', ylabel=y, title='Time Series of ' + title)
         ax.grid()
-        fig.savefig("./images/time_series.png")  # Save the figure instead of the axes
         plt.show()
-        
     else:
-        fig = px.line(df, x='time', y=y, title='Time Series of '+title)
+        fig = px.line(df, x='time', y=y, title='Time Series of ' + title)
         fig.show()
+
         
         
         
     
-def plot_time_series_dbd(df,y, title,matplotlib = False, first = True,):
+def plot_time_series_dbd(df,y, title,matplotlib = False):
     """
     Plot time series of people count, plot png if matplotlib = True
     :param df: pandas dataframe
@@ -79,9 +81,10 @@ def plot_time_series_dbd(df,y, title,matplotlib = False, first = True,):
     :param first: boolean, if True, it is the first time to plot, else not
     :return: None
     """
-    if first:
-        df.loc[:,'time'] = pd.to_datetime(df['time'])
-        df.loc[:,'date'] = df['time'].dt.date
+    # Always convert the 'time' column to datetime
+    
+    df.loc[:,'time'] = pd.to_datetime(df['time'])
+    df.loc[:,'date'] = df['time'].dt.date
         
     dates = df['date'].unique()
     
@@ -94,8 +97,6 @@ def plot_time_series_dbd(df,y, title,matplotlib = False, first = True,):
             ax.plot(df_date['time'], df_date[y])
             ax.set(xlabel='time', ylabel=y, title='Time Series of '+title)
             ax.grid()
-            # name the file with date as 'yyyy-mm-dd.png'
-            fig.savefig("./images/"+str(date)+".png")  # Save the figure instead of the axes
             plt.show()
             
         else:

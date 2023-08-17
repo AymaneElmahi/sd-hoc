6ac3ef10d9f03f55287e645faf760856fe8ef889
AymaneElmahi <elmahi.aymane@gmail.com>
2023-08-17 14:31:30 +0200

    added noise

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,7 @@ import plotly.express as px
 def json_to_pd(json_file):
     """
     extract data from json file and convert to pandas dataframe
-    :param json_file: json file
+    :param json_file: json file 
     :return: pandas dataframe
     """
     with open(json_file,encoding = 'utf-8') as js_file:
@@ -31,8 +31,6 @@ def create_df_(json_files):
         df_ = pd.concat([df_, json_to_pd(json_file)], ignore_index=True)
     return df_
 
-
-
 def epoch_to_datetime(epoch_time, h_add = 2):
     """
     Convert epoch time to datetime64 in GMT+2
@@ -43,8 +41,6 @@ def epoch_to_datetime(epoch_time, h_add = 2):
     datetime = np.datetime64(epoch_time, 'ms')  # Convert to datetime64 in GMT
     datetime += np.timedelta64(h_add, 'h')  # Add 2 hours for GMT+2
     return datetime
-
-
 
 def plot_time_series(df_, y_axis, title, matplotlib=False):
     """
@@ -95,6 +91,30 @@ def plot_time_series_dbd(df_,y_axis, title,matplotlib = False):
             fig = px.line(df__date, x='time', y= y_axis, title='Time Series of '+title)
             fig.show()
 
+def linear_interpolate_column(column):
+    """
+    Linearly interpolate missing values in a column
+    :param column: pandas Series
+    :return: pandas Series
+    """
+    column_copy = column.copy()
+    nan_indices = column_copy.index[column_copy.isna()]
+    for nan_index in nan_indices:
+        prev_valid_index = nan_index - 1
+        next_valid_index = nan_index + 1
+        while prev_valid_index >= 0 and pd.isna(column_copy[prev_valid_index]):
+            prev_valid_index -= 1
+        while next_valid_index < len(column_copy) and pd.isna(column_copy[next_valid_index]):
+            next_valid_index += 1
+        if prev_valid_index < 0 or next_valid_index >= len(column_copy):
+            continue
+        prev_valid_value = column_copy[prev_valid_index]
+        next_valid_value = column_copy[next_valid_index]
+        interpolation_ratio = (nan_index - prev_valid_index) / (next_valid_index - prev_valid_index)
+        interpolated_value=prev_valid_value+interpolation_ratio*(next_valid_value-prev_valid_value)
+        column_copy[nan_index] = interpolated_value
+    return column_copy
+
 def create_final_df(json_files, df_co2):
     """
     Create final dataframe with all the data.
@@ -114,8 +134,13 @@ def create_final_df(json_files, df_co2):
     # Transform epoch time to datetime, let the function know that the time is in GMT+1
     df_co2['timestamp'] = df_co2['epochtime'].apply(epoch_to_datetime, h_add=1)
     # Keep only 'timestamp' and non-null 'co2-ndir' columns
-    df_co2 = df_co2[['timestamp', 'co2-ndir']].dropna()
+    df_co2 = df_co2[['timestamp', 'co2-ndir', 'noise']]
     df_co2['days'] = df_co2['timestamp'].dt.date  # Extract only the date part
+    df_co2.noise = df_co2.noise.astype(str).str.replace(',','.').astype(float)
+    df_co2['co2-ndir'] = pd.to_numeric(df_co2['co2-ndir'], errors='coerce')
+    df_co2['noise'] = linear_interpolate_column(df_co2['noise'])
+    df_co2 = df_co2[df_co2['noise'].notna()]
+    df_co2 = df_co2[df_co2['co2-ndir'].notna()]
     # we are implementing this for a specific dataset, I had to delete the dates
     # after 2021-03-30 because they are not present in the 'peoplecount_rgb
     # feel free to adjust this part as needed
@@ -126,14 +151,14 @@ def create_final_df(json_files, df_co2):
     # drop days column
     df_co2 = df_co2.drop(columns=['days'])
     # rename columns
-    df_co2 = df_co2.rename(columns={'timestamp': 'time', 'co2-ndir': 'co2'})
+    df_co2 = df_co2.rename(columns={'timestamp': 'time', 'co2-ndir': 'co2','noise' : 'noise'})
     df_co2 = df_co2.sort_values('time')
     # Nearest Neighbor Matching
     df_matched = pd.merge_asof(df_, df_co2, on='time', direction='nearest')
     # Fuzzy Matching
     time_window = pd.Timedelta(minutes=1)  # Define the time window (adjust as needed)
     df_matched = pd.merge_asof(df_, df_co2, on='time', tolerance=time_window)
-    df_matched = df_matched[['time', 'peoplecount_rgb', 'co2']]
+    df_matched = df_matched[['time', 'peoplecount_rgb', 'co2','noise']]
     df_matched.dropna(inplace=True)
     # force co2 to be float
     df_matched['co2'] = pd.to_numeric(df_matched['co2'], downcast='float')
